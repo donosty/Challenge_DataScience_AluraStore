e675980781465802dc71455854ea8014ca9b4721
donosty <mzac0077@gmail.com>
2025-11-06 23:38:06 -0600

    first commit

---
+++ Codigo/challenge.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+url1 = '~/Descargas/CursoPython/Pandas/challenge/BD/tienda_1.csv'
+url2 = '~/Descargas/CursoPython/Pandas/challenge/BD/tienda_2.csv'
+url3 = '~/Descargas/CursoPython/Pandas/challenge/BD/tienda_3.csv'
+url4 = '~/Descargas/CursoPython/Pandas/challenge/BD/tienda_4.csv'
